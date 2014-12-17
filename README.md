e920c1fd92816d29aa9816b9e418320ea05a6171
Lance Turri <lance.turri@gmail.com>
2014-12-17 10:21:52 -0600

    Uploaded all of the scripts.
    Initial upload.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 JIRA-Modifications
 ==================
 
-These are modifications for the JIRA board view using CSS via stylish and JS via a userscript plugin.
+These are modifications for JIRA that eventually will extend to all of the board pages. Currently the agile board view is the only one that is fully modified, the rest are coming soon. All of this is done using CSS via stylish and JS via a userscript plugin.
+
+This was tested with Stylish and Tampermonkey on Chrome, and Stylish and Greasemonkey on Firefox.
---
+++ design-board.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name         JIRA Upgrades
+// @namespace    http://your.homepage/
+// @version      0.5
+// @description  To begin this will enable the clicking of a parent task to go straight to that task.
+// @author       Lance Turri
+// @match        http*://helpdesk.alkamitech.com/secure/RapidBoard.jspa?*
+// @grant        none
+// ==/UserScript==
+
+var timeoutID;
+
+// Set up the function that will delay the script loading until all calls are completed.
+function delayClickLoading () {
+    timeoutID = window.setTimeout(clickLoading, 1000); // One second works in most situations.
+}
+
+function clickLoading() {
+    jQuery(function() {
+        // Let's get font-awesome up in here!
+        var awesomey = '<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">';
+        jQuery('head').append(awesomey);
+        jQuery('.ghx-priority').addClass('fa fa-fw fa-level-up');
+        jQuery('.ghx-type[title="Design Task"]').addClass('fa fa-fw fa-paint-brush');
+        jQuery('.ghx-type[title="Bug"]').addClass('fa fa-fw fa-bug');
+        jQuery('.ghx-type[title="Feature Request"]').addClass('fa fa-fw fa-magic');
+        jQuery('.ghx-type[title="Sub-task"]').addClass('fa fa-fw fa-code-fork');
+        jQuery('.ghx-type[title="Story"]').addClass('fa fa-fw fa-book');
+        jQuery('.ghx-type[title="Improvement"]').addClass('fa fa-fw fa-line-chart');
+        jQuery('.ghx-type[title="Configuration Change"]').addClass('fa fa-fw fa-gear');
+
+        // Icons EVERYWHERE!
+        jQuery('#ghx-board-name').prepend('<span class="fa fa-fw fa-paint-brush" style="padding-right:3px;"></span>')
+
+        // On to the parent task search function. It's getting hot up in here!
+        jQuery('.ghx-parent-stub').on('click', function() {
+
+            // TODO: combine these tasks.
+            var taskTitle = $(this).attr('title'); // Get title attribute
+            taskTitle = taskTitle.split(" ", 1); // Split at the DEV number
+            taskTitle = taskTitle[0]; // Convert from Array to string.
+
+            // Inject the value into the search box and submit the form.
+            window.open('https://helpdesk.alkamitech.com/Browse/' + taskTitle);
+        });
+
+        // Now lets make the title of each task open in a new window if the link is clicked.
+        jQuery('.ghx-key a').on('click', function() {
+            window.open('https://helpdesk.alkamitech.com' + $(this).attr('href'));
+        });
+    });
+}
+
+// Load the click listener.
+delayClickLoading();
+
