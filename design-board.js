4f9d6395f7910c0bcf453ba5cb46ae75855b1e22
Lance Turri <lance.turri@gmail.com>
2015-01-06 11:24:26 -0600

    Updated JS and Agile Board Theme
    Made corrections to the javascript file to run on window load and on
    every AJAX refresh as well as fixed some bugs in the CSS.

--- design-board.js
+++ design-board.js
@@ -8,23 +8,21 @@
 // @grant        none
 // ==/UserScript==
 
-var timeoutID;
+jQuery(window).load(function () {
+    // First, there was a new font
+    var font = '<link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet" type="text/css">';
+    jQuery('head').append(font);
 
-// Set up the function that will delay the script loading until all calls are completed.
-function delayClickLoading () {
-    timeoutID = window.setTimeout(clickLoading, 1000); // One second works in most situations.
-}
+    // Let's get font-awesome up in here!
+    var awesomey = '<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">';
+    jQuery('head').append(awesomey);
 
-function clickLoading() {
-    jQuery(function() {
-        // First, there was a new font
-        var font = '<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet" type="text/css">'
-        jQuery('head').append(font);
+    // Icons EVERYWHERE!
+    jQuery('#ghx-board-name').prepend('<span class="fa fa-fw fa-paint-brush" style="padding-right:3px;"></span>');
 
-        // Let's get font-awesome up in here!
-        var awesomey = '<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">';
-        jQuery('head').append(awesomey);
-        jQuery('.ghx-priority').addClass('fa fa-fw fa-level-up');
+    // Main function to be rerun upon ajax complete.
+    var magic = function() {
+        //jQuery('.ghx-priority').addClass('fa fa-fw fa-level-up');
         jQuery('.ghx-type[title="Design Task"]').addClass('fa fa-fw fa-paint-brush');
         jQuery('.ghx-type[title="Bug"]').addClass('fa fa-fw fa-bug');
         jQuery('.ghx-type[title="Feature Request"]').addClass('fa fa-fw fa-magic');
@@ -33,15 +31,11 @@ function clickLoading() {
         jQuery('.ghx-type[title="Improvement"]').addClass('fa fa-fw fa-thumbs-up');
         jQuery('.ghx-type[title="Configuration Change"]').addClass('fa fa-fw fa-gear');
 
-        // Icons EVERYWHERE!
-        jQuery('#ghx-board-name').prepend('<span class="fa fa-fw fa-paint-brush" style="padding-right:3px;"></span>')
-
         // On to the parent task search function. It's getting hot up in here!
-        jQuery('.ghx-parent-stub').on('click', function() {
-
+        jQuery('.ghx-parent-stub').on('click', function (e) {
             // TODO: combine these tasks.
             var taskTitle = $(this).attr('title'); // Get title attribute
-            taskTitle = taskTitle.split(" ", 1); // Split at the DEV number
+            taskTitle = taskTitle.split(' ', 1); // Split at the DEV number
             taskTitle = taskTitle[0]; // Convert from Array to string.
 
             // Inject the value into the search box and submit the form.
@@ -49,11 +43,18 @@ function clickLoading() {
         });
 
         // Now lets make the title of each task open in a new window if the link is clicked.
-        jQuery('.ghx-key a').on('click', function(e) {
+        jQuery('.ghx-key a').on('click', function (e) {
             window.open('https://helpdesk.alkamitech.com' + $(this).attr('href'));
+            return false;
         });
-    });
-}
+    };
 
-// Load the click listener.
-delayClickLoading();
+    // Run magic upon the first load.
+    //All other subsequent loads will be taken care of by ajaxComplete.
+    magic();
+
+    // Bind magic() to ajaxComplete
+    jQuery(document).ajaxComplete(magic);
+
+
+});
